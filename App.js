ba81ae84eda025273f462f1015810cc1cd5468a3
Muhammad Mubashar <mubashar1030@hotmail.com>
2020-04-24 03:05:02 +0500

    Change User Type Screen Header

--- App.js
+++ App.js
@@ -1,36 +1,52 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Colors from './constants/colors.js';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import UserTypeScreen from './screens/UserTypeScreen.js';
+import Colors from "./constants/colors.js";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import UserTypeScreen from "./screens/UserTypeScreen.js";
 
 const Stack = createStackNavigator();
 
-const getFont = () => Font.loadAsync({
-  'Helvetica': require('./assets/fonts/SundayMorning.otf'),
-});
+const getFont = () =>
+  Font.loadAsync({
+    Helvetica: require("./assets/fonts/SundayMorning.otf"),
+  });
 
 export default function App() {
   const [fontsLoaded, setfontsLoaded] = useState(false);
+
+  // Since fonts are loaded asynchronously the below condition checks if
+  // fonts are loaded and only starts the app once the fonts are loaded so
+  // so that the components do not get rendered before the loading of font.
   if (fontsLoaded) {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={UserTypeScreen} />
+          <Stack.Screen
+            name="Home"
+            component={UserTypeScreen}
+            options={{
+              title: "User Type",
+              headerStyle: {
+                backgroundColor: Colors.primary,
+              },
+              // headerTitleAlign: 'center',
+              headerTitleStyle: {
+                fontWeight: "bold",
+                fontSize: 22,
+                color: Colors.tertiary,
+              },
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     );
-  }
-  else {
+  } else {
     return (
-      <AppLoading
-        startAsync={getFont}
-        onFinish={() => setfontsLoaded(true)}
-      />
+      <AppLoading startAsync={getFont} onFinish={() => setfontsLoaded(true)} />
     );
   }
 }
@@ -38,8 +54,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
