3bc5897a56804e08d98fad1a947521f6c3eb0c78
Muhammad Mubashar <mubashar1030@hotmail.com>
2020-04-24 02:25:55 +0500

    Add Helvetica Font

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Colors from './constants/colors.js';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 import UserTypeScreen from './screens/UserTypeScreen.js';
 
 const Stack = createStackNavigator();
 
+const getFont = () => Font.loadAsync({
+  'Helvetica': require('./assets/fonts/SundayMorning.otf'),
+});
+
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={UserTypeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [fontsLoaded, setfontsLoaded] = useState(false);
+  if (fontsLoaded) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={UserTypeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  else {
+    return (
+      <AppLoading
+        startAsync={getFont}
+        onFinish={() => setfontsLoaded(true)}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
