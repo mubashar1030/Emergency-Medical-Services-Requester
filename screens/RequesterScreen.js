782a8f5f01139940f29d070b90f694fc7b28705d
Muhammad Mubashar <mubashar1030@hotmail.com>
2020-04-25 20:08:04 +0500

    Add bottom tabs in requester screen

--- screens/RequesterScreen.js
+++ screens/RequesterScreen.js
@@ -8,18 +8,73 @@ import {
   TextInput,
   ScrollView,
   Alert,
+  Modal,
 } from "react-native";
 import Colors from "../constants/colors.js";
 import {
   widthPercentageToDP,
   heightPercentageToDP,
 } from "react-native-responsive-screen";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const RequesterScreen = ({ route, navigation }) => {
+
+  function HomeScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
+
+  function SettingsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            return (
+              <Entypo
+                name="plus"
+                size={widthPercentageToDP("12%")}
+                color={Colors.primary}
+              />
+            );
+          } else if (route.name === "Settings") {
+            return (
+              <Ionicons
+                name="md-settings"
+                size={widthPercentageToDP("8%")}
+                color={Colors.secondary}
+              />
+            );
+          }
+
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {backgroundColor: Colors.tertiary},
+        inactiveBackgroundColor: Colors.tertiary,
+        tabStyle: { elevation: 4, borderRadius: 10 },
+        activeBackgroundColor: Colors.selected,
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 };
 
@@ -28,6 +83,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: Colors.tertiary,
   },
 });
 
