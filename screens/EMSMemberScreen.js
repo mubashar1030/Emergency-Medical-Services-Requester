7c2c3210623f5e2f3c62ce7da83ff904b29a9fc4
Muhammad Mubashar <mubashar1030@hotmail.com>
2020-04-27 06:45:18 +0500

    Create EMS Member Screen

---
+++ screens/EMSMemberScreen.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ScrollView,
+  Alert,
+  Modal,
+} from "react-native";
+import Colors from "../constants/colors.js";
+import {
+  widthPercentageToDP,
+  heightPercentageToDP,
+} from "react-native-responsive-screen";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SettingsScreen from './SettingsScreen'
+import AcceptRequestScreen from './AcceptRequestScreen'
+
+const EMSMemberScreen = ({ route, navigation }) => {
+
+  const Tab = createBottomTabNavigator();
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "AcceptRequestScreen") {
+            return (
+              <Entypo
+                name="plus"
+                size={widthPercentageToDP("12%")}
+                color={Colors.primary}
+              />
+            );
+          } else if (route.name === "Settings") {
+            return (
+              <Ionicons
+                name="md-settings"
+                size={widthPercentageToDP("8%")}
+                color={Colors.secondary}
+              />
+            );
+          }
+
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {backgroundColor: Colors.tertiary},
+        inactiveBackgroundColor: Colors.tertiary,
+        tabStyle: { elevation: 4, borderRadius: 10 },
+        activeBackgroundColor: Colors.selected,
+      }}
+    >
+      <Tab.Screen name="AcceptRequestScreen" component={AcceptRequestScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.tertiary,
+  },
+});
+
+export default EMSMemberScreen;
