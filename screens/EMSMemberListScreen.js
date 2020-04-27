398a50b4fb10aadfdce04e8edf351d861fffef29
Muhammad Mubashar <mubashar1030@hotmail.com>
2020-04-28 01:29:32 +0500

    Added Add member in EMSMemberList Screen

--- screens/EMSMemberListScreen.js
+++ screens/EMSMemberListScreen.js
@@ -42,20 +42,167 @@ const AcceptRequestScreen = () => {
       photo: require("../assets/dummy.png"),
     },
   ]);
+
+  const [newMemberName, setNewMemberName] =useState('');
+  const [newMemberEmail, setNewMemberEmail] =useState('');
+  const [newMemberPhone, setNewMemberPhone] =useState('');
+  const [newMemberPassword, setNewMemberPassword] =useState('');
+  const [newMemberConfirmPassword, setNewMemberConfirmPassword] =useState('');
+
+  const makeAdminHandler = () => {
+    Alert.alert("Are you sure?", "", [
+      {
+        text: "Yes",
+        style: "cancel",
+      },
+      {
+        text: "No",
+        style: "cancel",
+      },
+    ]);
+  };
+
+  const removeMemberHandler = () => {
+    Alert.alert("Are you sure?", "", [
+      {
+        text: "Yes",
+        style: "cancel",
+      },
+      {
+        text: "No",
+        style: "cancel",
+      },
+    ]);
+  };
+
+  const addMemberHandler = () => {
+    if (newMemberPassword !== newMemberConfirmPassword){
+      Alert.alert("Password do not match", "", [
+        {
+          text: "Okay",
+          style: "cancel",
+        },
+      ]);
+    }
+  }
+
   let content;
 
   if (isAddNewMember) {
     content = (
-      <View style={styles.container}>
-        <Text>Hello</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.container}>
+          <View style={styles.addMemberContaniner}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Name"
+                onChangeText={(text) => setNewMemberName(text)}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Email"
+                onChangeText={(text) => setNewMemberEmail(text)}
+                autoCapitalize="none"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Phone"
+                onChangeText={(text) => setNewMemberPhone(text)}
+                autoCapitalize="none"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Password"
+                onChangeText={(text) => setNewMemberPassword(text)}
+                autoCapitalize="none"
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Confirm Password"
+                onChangeText={(text) => setNewMemberConfirmPassword(text)}
+                autoCapitalize="none"
+              />
+            </View>
+          </View>
+          <View style={styles.buttonContainer2}>
+            <MyButton onPress={() => addMemberHandler()} text="Add Member" />
+          </View>
+          <TouchableOpacity
+            style={{ alignItems: "center" }}
+            onPress={()=>setIsAddNewMember(false)}
+          >
+            <Text style={styles.signupText}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     );
   } else if (isMemberSelected) {
     content = (
       <View style={styles.container}>
-        <Photo photo={memberSelected.photo} photoContainerStyle={{marginTop: '20%'}}/>
+        <Photo
+          photo={memberSelected.photo}
+          photoContainerStyle={{ marginTop: "20%" }}
+        />
         <View style={styles.memberInformationContainer}>
-
+          <View style={styles.memberDetails}>
+            <InfoText
+              label="Name: "
+              text={memberSelected.name}
+              border={1}
+              labelWidth={widthPercentageToDP("18%")}
+              fontSize={16}
+            />
+            <InfoText
+              label="Email: "
+              text={memberSelected.email}
+              border={1}
+              labelWidth={widthPercentageToDP("18%")}
+              fontSize={16}
+            />
+            <InfoText
+              label="Phone: "
+              text={memberSelected.phone}
+              border={1}
+              labelWidth={widthPercentageToDP("18%")}
+              fontSize={16}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <MyButton
+              buttonStyle={{
+                width: "40%", //widthPercentageToDP("25%"),
+                height: "70%", //heightPercentageToDP("5%"),
+              }}
+              text="Make Admin"
+              textStyle={{ fontSize: 17 }}
+              onPress={() => makeAdminHandler()}
+            />
+            <MyButton
+              buttonStyle={{
+                width: "40%", //widthPercentageToDP("25%"),
+                height: "70%", //heightPercentageToDP("5%"),
+              }}
+              text="Remove"
+              textStyle={{ fontSize: 17 }}
+              onPress={() => removeMemberHandler()}
+            />
+          </View>
+        </View>
+        <View style={styles.endButton}>
+          <MyButton
+            buttonStyle={{
+              width: widthPercentageToDP("25%"),
+              height: heightPercentageToDP("5%"),
+              backgroundColor: Colors.primary,
+            }}
+            text="Back"
+            textStyle={{ fontSize: widthPercentageToDP("5%") }}
+            onPress={() => setIsMemberSelected(false)}
+          />
         </View>
       </View>
     );
@@ -77,7 +224,7 @@ const AcceptRequestScreen = () => {
               fontSize: widthPercentageToDP("4%"),
               color: Colors.secondary,
             }}
-            // onPress={() => setIsRequestAccepted(false)}
+            onPress={() => setIsAddNewMember(true)}
           />
         </View>
         <FlatList
@@ -127,15 +274,20 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
     backgroundColor: Colors.tertiary,
   },
+  scrollContainer: {
+    paddingVertical: heightPercentageToDP("10%"),
+    alignItems: "center",
+    justifyContent: "center",
+  },
   titleContainer: {
-    width: '90%',//widthPercentageToDP("90%"),
-    marginTop: '2%',//heightPercentageToDP("2%"),
-    marginBottom: '1%',//heightPercentageToDP("1%"),
+    width: "90%", //widthPercentageToDP("90%"),
+    marginTop: "2%", //heightPercentageToDP("2%"),
+    marginBottom: "1%", //heightPercentageToDP("1%"),
   },
   title: {
     fontFamily: "Helvetica",
     color: Colors.primary,
-    fontSize: 32,//heightPercentageToDP("4%"),
+    fontSize: 32, //heightPercentageToDP("4%"),
     fontWeight: "bold",
   },
   card: {
@@ -164,17 +316,66 @@ const styles = StyleSheet.create({
   addMemberButton: {
     width: widthPercentageToDP("82%"),
     flexDirection: "row-reverse",
-    marginTop: '1%',//heightPercentageToDP("1%"),
-    marginBottom: '1%'//heightPercentageToDP("1%"),
+    marginTop: "1%", //heightPercentageToDP("1%"),
+    marginBottom: "1%", //heightPercentageToDP("1%"),
   },
   memberInformationContainer: {
-    width: widthPercentageToDP('90%'),
-    height: '40%',//heightPercentageToDP('40%'),
+    width: widthPercentageToDP("90%"),
+    height: "35%", //heightPercentageToDP('40%'),
     elevation: 4,
     backgroundColor: Colors.tertiary,
-    borderRadius: widthPercentageToDP('7%'),
-    marginTop: heightPercentageToDP('3%')
-  }
+    borderRadius: widthPercentageToDP("7%"),
+    marginTop: heightPercentageToDP("3%"),
+    paddingVertical: "3%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  memberDetails: {
+    width: "100%",
+    paddingHorizontal: "6%",
+  },
+  buttonContainer: {
+    // backgroundColor: 'blue',
+    width: "88%",
+    height: "30%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: "5%",
+  },
+  endButton: {
+    width: widthPercentageToDP("90%"),
+    flexDirection: "row-reverse",
+    marginTop: "10%", //heightPercentageToDP("4%"),
+  },
+  addMemberContaniner: {
+    width: widthPercentageToDP("90%"),
+    height: heightPercentageToDP("50%"),
+    justifyContent: "space-evenly",
+    // paddingTop: heightPercentageToDP("6%"),
+    backgroundColor: Colors.tertiary,
+    alignItems: "center",
+    // elevation: 4,
+    // borderRadius: 25
+  },
+  inputContainer: {
+    width: "85%", //widthPercentageToDP("70%"),
+    height: "12%", //heightPercentageToDP("7%"),
+    backgroundColor: Colors.tertiary,
+    borderRadius: widthPercentageToDP("5%"),
+    elevation: 4,
+    justifyContent: "center",
+    paddingLeft: heightPercentageToDP("3%"),
+  },
+  buttonContainer2: {
+    marginTop: "3%",
+    marginBottom: '3%'
+  },
+  signupText: {
+    fontFamily: "Helvetica",
+    fontSize: 25,
+    color: Colors.secondary,
+  },
 });
 
 export default AcceptRequestScreen;
